c71549dbb8cc3401bae72ce88586188dccd969ad
seunope <seunope2004@yahoo.co.uk>
2025-03-19 09:44:29 +0100

    set up validators

--- src/module/v1/tasks/tasks.dto.ts
+++ src/module/v1/tasks/tasks.dto.ts
@@ -1,9 +1,19 @@
+import { IsString, IsNotEmpty, IsInt, IsIn } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
   description: string;
+
+  @IsInt()
   user_id: number;
 }
 
 export class UpdateTaskStatusDto {
+  @IsString()
+  @IsIn(['pending', 'in-progress', 'completed'])
   status: string;
 }
